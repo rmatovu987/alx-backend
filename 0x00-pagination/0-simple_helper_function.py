49c4912fa81912c89f1e11e1aa6defdf5a50c161
rmatovu987 <rmatovu987@gmail.com>
2022-05-02 16:54:32 +0300

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Write a function named index_range that takes two integer arguments page and page_size.
+Write a function named index_range that takes two
+integer arguments page and page_size.
 """
 
 
