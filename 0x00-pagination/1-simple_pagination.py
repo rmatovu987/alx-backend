0eb203da485bf5d69cf199d95f6fbbd946682730
rmatovu987 <rmatovu987@gmail.com>
2022-05-02 17:02:32 +0300

    Task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,7 +28,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
             Implement a method named get_page that takes two integer
-            arguments page with default value 1 and page_size with default value 10.
+            arguments page with default value 1 and page_size with
+            default value 10.
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
