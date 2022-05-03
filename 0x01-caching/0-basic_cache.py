60cd30a246378594ab8f3273c09bd15594702fc0
rmatovu987 <rmatovu987@gmail.com>
2022-05-03 14:27:34 +0300

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicCache that
+inherits from BaseCaching and is a caching system:
+"""
+from abc import ABC
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching, ABC):
+    """Basic Cache class"""
+
+    def put(self, key, item):
+        """Assign the dictionary"""
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+
+    def get(self, key, item):
+        """Get item"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
