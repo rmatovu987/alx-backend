b8e35dc757061c38c616e1d459a13c12fe40bfe9
rmatovu987 <rmatovu987@gmail.com>
2022-05-03 14:33:35 +0300

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""
-Create a class BasicCache that
-inherits from BaseCaching and is a caching system:
-"""
+""" caching system
+    """
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Basic Cache class"""
+    """ caching system:
+    Args:
+        BasicCachey ([class]): [basic caching]
+    """
 
     def put(self, key, item):
-        """Assign the dictionary"""
+        """ Add an item in the cache
+        """
         if not (key is None or item is None):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get item"""
+        """ Get an item by key
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
