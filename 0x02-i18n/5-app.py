dab9afec8bceeb33607c297ca186ab23482d4bb9
rmatovu987 <rmatovu987@gmail.com>
2022-05-10 19:34:12 +0300

    Task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,9 +4,17 @@ This is the main file of the flask application.
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
+from typing import Union
 
 app = Flask(__name__)
 babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
@@ -19,19 +27,43 @@ class Config(object):
 
 
 app.config.from_object(Config)
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 
-def get_user():
-    """Returns a user dictionary or None if doesnt exist"""
-    userId = request.args.get('login_as')
-    if userId and userId.isdigit():
-        users.get(int(userId), None)
+@babel.localeselector
+def get_locale() -> str:
+    """
+    This function is used to select the language.
+
+    Returns:
+        str: The language.
+    """
+    locale = request.args.get('locale')
+    if locale:
+        if locale in app.config['LANGUAGES']:
+            return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """
+    This is the main page of the flask application.
+
+    Returns:
+        str: The rendered template.
+    """
+    return render_template('5-index.html')
+
+
+def get_user() -> Union[dict, None]:
+    """
+    This function is used to get the user.
+    Returns:
+        dict: The user.
+    """
+    login_as = request.args.get('login_as')
+    if login_as and login_as.isdigit():
+        return users.get(int(login_as), None)
     return None
 
 
@@ -44,31 +76,6 @@ def before_request() -> None:
         None
     """
     g.user = get_user()
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """
-    This function is used to select the language.
-
-    Returns:
-        str: The language.
-    """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """
-    This is the main page of the flask application.
-
-    Returns:
-        str: The rendered template.
-    """
-    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
