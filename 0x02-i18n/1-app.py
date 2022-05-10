4579eb8fd0a522d59e174a553474cc6465579980
rmatovu987 <rmatovu987@gmail.com>
2022-05-10 18:54:02 +0300

    Task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -15,7 +15,7 @@ class Config(object):
     This is the configuration class
     """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE= 'en'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
